c02c9e4db1ec4fa2d80fc979c839acfe326606ea
Benjamin Wild <b.w@fu-berlin.de>
2015-12-10 19:14:51 +0100

    add skeleton for new trackers

---
+++ BaseTracker.cpp
@@ -0,0 +1,29 @@
+#include "BaseTracker.h"
+
+#include <QApplication>
+#include <QIntValidator>
+#include <QPushButton>
+#include <QPainter>
+
+#include <biotracker/TrackingAlgorithm.h>
+#include <biotracker/Registry.h>
+
+extern "C" {
+    void registerTracker() {
+        BioTracker::Core::Registry::getInstance().registerTrackerType<BaseTracker>("BaseTracker");
+    }
+}
+
+BaseTracker::BaseTracker(Settings &settings)
+    : TrackingAlgorithm(settings)
+    , m_toolsFrame(std::make_shared<QFrame>()) {
+}
+
+void BaseTracker::track(ulong, const cv::Mat &imgOriginal) {
+}
+
+void BaseTracker::paint(cv::Mat &image, const TrackingAlgorithm::View &view) {
+}
+
+void BaseTracker::paintOverlay(QPainter *painter) {
+}
---
+++ BaseTracker.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <QGroupBox>
+#include <QFormLayout>
+#include <QLineEdit>
+#include <QPushButton>
+
+#include <biotracker/TrackingAlgorithm.h>
+
+class BaseTracker : public TrackingAlgorithm {
+    Q_OBJECT
+  public:
+    BaseTracker(Settings &settings);
+
+    void track(ulong frameNumber, const cv::Mat &frame) override;
+    void paint(cv::Mat &m, View const &view = OriginalView) override;
+    void paintOverlay(QPainter *painter) override;
+
+    void postConnect() override;
+
+    std::shared_ptr<QWidget> getToolsWidget() override;
+
+  public Q_SLOTS:
+    void changeFilterColor();
+
+  private:
+    std::shared_ptr<QFrame> m_toolsFrame;
+};
